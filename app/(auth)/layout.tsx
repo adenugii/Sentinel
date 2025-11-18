08e9e5f1c372180679c5c40fb0ce5219d0fc52b6
Dzvelocity <dzikrimurtadlo@gmail.com>
2025-11-18 14:05:57 +0700

    add(ui): asset for landing and auth page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -6,7 +6,7 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen flex">
+    <div className="h-full absolute inset-0 flex">
       {/* Kolom Kiri: Grafis (hanya tampil di layar besar) */}
       <AuthGraphic />
 
